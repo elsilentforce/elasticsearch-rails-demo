e3ff1bf15efc3610315546d67a2086e43e5deb45
Patricio Tapia <native.devil@gmail.com>
2022-04-10 17:24:13 -0400

    REFACT: Field typo

--- db/migrate/20220410211303_create_users.rb
+++ db/migrate/20220410211303_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :email, null: false, unique: true  
-      t.string :name
-      t.string :lastname
+      t.string :first_name, null: false
+      t.string :last_name, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_10_211303) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
-    t.string "name"
-    t.string "lastname"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
