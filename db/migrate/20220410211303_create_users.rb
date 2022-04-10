5c9da4eacef6066e9cd7f8159b30a6439189eb5c
Patricio Tapia <native.devil@gmail.com>
2022-04-10 17:17:39 -0400

    ADD: Users config

--- db/migrate/20220410211303_create_users.rb
+++ db/migrate/20220410211303_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :email, null: false, unique: true
+      t.string :email, null: false, unique: true  
       t.string :name
       t.string :lastname
 
